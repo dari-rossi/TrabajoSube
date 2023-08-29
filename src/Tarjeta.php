d9172c7e6b7407671fd48e752c8e3d68c1f29270
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-08-29 17:30:20 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,1 +1,14 @@
-
+<?php
+namespace TrabajoSube;
+class Tarjeta{
+    protected $saldo;
+    
+    public function __construct($linea){
+        $this->linea = $linea;
+    }
+    
+    //    Funcion de ejemplo para test
+    public function getLinea(){
+        return $this->linea;
+    }
+}
