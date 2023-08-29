770f3a359e0fb0c3f1e83f6a5d8997759b40b733
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-08-29 17:15:50 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -1,1 +1,14 @@
-
+<?php
+namespace TrabajoSube;
+class Boleto{
+    protected $tarifa;
+    
+    public function __construct($linea){
+        $this->linea = $linea;
+    }
+    
+    //    Funcion de ejemplo para test
+    public function getLinea(){
+        return $this->linea;
+    }
+}
