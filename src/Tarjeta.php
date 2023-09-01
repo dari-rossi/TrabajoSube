d18e662bde2db3147088b8adce1d81437c630965
Lorenzo393 <loloc.gelos@outlook.com>
2023-09-01 12:49:16 -0300

    Agregada la funcion cargar_tarjeta en la clase tarjeta

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,8 +1,20 @@
 <?php
 namespace TrabajoSube;
 class Tarjeta{
-    protected $saldo;
-    
+    public $saldo;
+    public $cargas_aceptadas = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
+    private $limite_saldo = 6600;
+
+    public function cargar_tarjeta($nueva_carga){
+        if(in_array($nueva_carga , $cargas_aceptadas) && ($saldo + $nueva_carga) <= limite_saldo){
+            printf("Su saldo se a cargado correctamente\nSu nuevo saldo es de %i",$this->saldo);
+            return $this->saldo += nueva_carga;
+        }
+        else{
+            printf("Error al cargar el monto ingresado\nVerifique que el monto ingresado este dentro de los montos aceptados\nSu saldo es de %i",$this->saldo);
+        }
+    }
+
     public function __construct($saldo=0){
         $this->saldo = $saldo;
     }
