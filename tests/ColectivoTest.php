671c8875d7393ed7a5510d6aaa996f4732314d13
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-05 17:06:57 -0300

    Update ColectivoTest.php

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,8 +6,10 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase{
 
-    public function testGetlinea(){
-        $cole = new Colectivo(103);
-        $this->assertEquals($cole->getLinea(), 103);
+    public function testPagarCon(){
+        $cole = new Colectivo();
+        $tarjeta = new Tarjeta(120);
+        $cole->pagarCon($tarjeta);
+        $this->assertEquals($tarjeta->saldo, 0);
     }
 }
