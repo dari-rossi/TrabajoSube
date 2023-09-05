97b4f96eb193330969ddc0bd64fd457fc2fb8309
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-05 17:09:55 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -5,16 +5,16 @@ class Colectivo{
     public $costo_boleto = 120;
     
     public function pagarCon($tarjeta){
-        if($tarjeta->saldo >= $costo_boleto){
-            $tarjeta->saldo -= $costo_boleto;
+        if($tarjeta->saldo >= $this->costo_boleto){
+            $tarjeta->saldo -= $this->costo_boleto;
             echo "Su boleto ha sido pagado exitosamente";
-            echo "Su saldo es de " . $this->saldo;
+            echo "Su saldo es de " . $tarjeta->saldo;
             return new Boleto($costo_boleto);
         }
             
         else{
             echo "No tiene suficiente saldo para comprar un boleto";
-            echo "Su saldo es de " . $this->saldo;
+            echo "Su saldo es de " . $tarjeta->saldo;
             echo "El costo del boleto es de " . $this->costo_boleto;
         }
     }
