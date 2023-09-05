55bef9d625f0800eb904283a619e63a0b42c2822
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-05 16:57:33 -0300

    Update Colectivo.php

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,40 +1,22 @@
 <?php
 namespace TrabajoSube;
 
-include Tarjeta.php;
-include Boleto.php;
-
 class Colectivo{
-    protected $linea;
     public $costo_boleto;
     
-    //    Funcion de ejemplo para test
-    public function getLinea(){
-        return $this->linea;
-    }
-
-    /*
-    public function cobrarBoleto($tarjeta,$boleto){
-        if ($tarjeta->saldo > $boleto->tarifa){
-            $tarjeta->saldo -= $boleto->tarifa;
-        }
-        else echo "No hay saldo suficiente";
-    }
-    */
-
     public function pagarCon($tarjeta){
         if($tarjeta->saldo >= $costo_boleto){
             $tarjeta->saldo -= $costo_boleto;
-            printf("Su boleto ha sido pagado exitosamente\nSu saldo es de %i",$this->saldo);
+            echo "Su boleto ha sido pagado exitosamente";
+            echo "Su saldo es de " . $this->saldo;
             return new Boleto($costo_boleto);
         }
+            
         else{
-            printf("No tiene suficiente saldo para comprar un boleto\nSu saldo es de %i\nEl costo del boleto es de %i",$this->saldo,$this->costo_boleto);
+            echo "No tiene suficiente saldo para comprar un boleto";
+            echo "Su saldo es de " . $this->saldo;
+            echo "El costo del boleto es de " . $this->costo_boleto;
         }
-    }
-
-    public function __construct($linea){
-        $this->linea = $linea;
     }
 
 }
