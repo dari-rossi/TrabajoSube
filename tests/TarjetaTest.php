2b7a51a3b0aa3b90e293f614d513e032d8b50063
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-05 17:32:14 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -8,8 +8,8 @@ class TarjetaTest extends TestCase{
 
     public function testCargar_tarjeta(){
       $tarjeta = new Tarjeta(0);
-      $tarjeta->cargarTarjeta(263);
-      $tarjeta->cargarTarjeta(300);
+      $tarjeta->cargar_tarjeta(263);
+      $tarjeta->cargar_tarjeta(300);
       $this->assertEquals($tarjeta->saldo, 300);
     }
 
