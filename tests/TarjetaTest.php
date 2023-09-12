90f36aad9980b9ed0bd05a321c05fe978985eaef
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-12 17:47:03 -0300

    Update TarjetaTest.php
    Corrección del Test tras la implementación del saldo acumulado superando a 6600

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -9,8 +9,9 @@ class TarjetaTest extends TestCase{
     public function testCargar_tarjeta(){
       $tarjeta = new Tarjeta(0);
       $tarjeta->cargar_tarjeta(263);
-      $tarjeta->cargar_tarjeta(300);
-      $this->assertEquals($tarjeta->saldo, 300);
+      $tarjeta->cargar_tarjeta(4000);
+      $tarjeta->cargar_tarjeta(4000);
+      $this->assertEquals($tarjeta->saldo, 6600);
     }
 
     public function test__construct(){
