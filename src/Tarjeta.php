2a37716ef0f898e6126294e2d7c2abc3ba14b094
Lorenzo Correa <64044513+Lorenzo393@users.noreply.github.com>
2023-09-19 13:41:06 -0300

    Agrego que cargue el saldo de la tarjeta además de calcular el acumulado

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -1,11 +1,13 @@
 <?php
 namespace TrabajoSube;
 class Tarjeta{
-    public $saldo;
     public $cargas_aceptadas = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
     private $limite_saldo = 6600;
-    public $saldo_acumulado;
+    public $saldo_acumulado = 0;
+    public $saldo;
 
+    //Falta agregar la restitucion del saldo acumulado;
+    
     public function cargar_tarjeta($nueva_carga){
         if(in_array($nueva_carga , $this->cargas_aceptadas)){
             if(($this->saldo + $nueva_carga) <= $this->limite_saldo){
@@ -14,7 +16,11 @@ class Tarjeta{
             $this->saldo += $nueva_carga;
             }
             else{
-                $this->saldo_acumulado += $this->saldo + $nueva_carga - $this->limite_saldo
+                $this->saldo_acumulado += $this->saldo + $nueva_carga - $this->limite_saldo;
+                $this->saldo = $this->limite_saldo;
+                echo "Su saldo se ha cargado correctamente ";
+                echo "Su nuevo saldo es de " . $this->saldo;
+                echo "Su saldo acumulado es de " . $this->saldo_acumulado;
                 }
         }
         else{
