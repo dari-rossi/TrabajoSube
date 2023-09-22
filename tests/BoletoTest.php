4a308c57a2129d9384241e78010447900cff702c
Lorenzo393 <loloc.gelos@outlook.com>
2023-09-22 10:45:12 -0300

    Arreglo test boleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -7,7 +7,10 @@ use PHPUnit\Framework\TestCase;
 class BoletoTest extends TestCase{
 
     public function test__construct(){
-        $boleto = new Boleto(250);
+        
+        $boleto = new Boleto(250 , 320);
+
         $this->assertEquals($boleto->tarifa, 250);
+        $this->assertEquals($boleto->saldo, 320);
     }
 }
