cf13ff69aefac0f7387b4365f3884555ca6e9c06
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-26 13:10:36 -0300

    Update FranquiciaCompletaJubilados.php

--- src/FranquiciaCompletaJubilados.php
+++ src/FranquiciaCompletaJubilados.php
@@ -2,7 +2,7 @@
 namespace TrabajoSube;
 
 class FranquiciaCompletaJubilados extends Tarjeta{
-  public $tarifa_descuento = 0
+  public $tarifa_descuento = 0;
   public $tipo = "FranquiciaCompletaJubilados"
   }
 }
