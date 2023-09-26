75fd8e92dbdb7d8db6af5c93e4625215f5b7123c
Lorenzo Correa <64044513+Lorenzo393@users.noreply.github.com>
2023-09-26 16:26:24 -0300

    Update and rename FranquiciaParcialMBE.php to FranquiciaParcialMBEyU.php
    Arreglando nombre

--- src/FranquiciaParcialMBE.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace TrabajoSube;
-
-class FranquiciaParcialMBE extends Tarjeta{
-  public $tarifa_descuento = $tarifa /2;
-  public $tipo = "FranquiciaParcialMBE"
-  }
-}
---
+++ src/FranquiciaParcialMBEyU.php
@@ -0,0 +1,8 @@
+<?php
+namespace TrabajoSube;
+
+class FranquiciaParcialMBEyU extends Tarjeta{
+  public $tarifa_descuento = $tarifa /2;
+  public $tipo = "FranquiciaParcialMBEyU"
+  }
+}
