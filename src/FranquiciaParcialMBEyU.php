ca40c512bba83a5e0e9999085bf958eb5b973add
Lorenzo Correa <64044513+Lorenzo393@users.noreply.github.com>
2023-09-26 16:28:24 -0300

    Update FranquiciaParcialMBEyU.php

--- src/FranquiciaParcialMBEyU.php
+++ src/FranquiciaParcialMBEyU.php
@@ -3,6 +3,6 @@ namespace TrabajoSube;
 
 class FranquiciaParcialMBEyU extends Tarjeta{
   public $tarifa_descuento = $tarifa /2;
-  public $tipo = "FranquiciaParcialMBEyU"
+  public $tipo = "FranquiciaParcialMBEyU";
   }
 }
