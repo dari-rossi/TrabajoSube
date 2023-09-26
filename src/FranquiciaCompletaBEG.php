4aa9df327633984bfa60067b16e9842c1203263c
Dari :p <104741733+dari-rossi@users.noreply.github.com>
2023-09-26 13:10:26 -0300

    Update FranquiciaCompletaBEG.php

--- src/FranquiciaCompletaBEG.php
+++ src/FranquiciaCompletaBEG.php
@@ -2,7 +2,7 @@
 namespace TrabajoSube;
 
 class FranquiciaCompletaBEG extends Tarjeta{
-  public $tarifa_descuento = 0
+  public $tarifa_descuento = 0;
   public $tipo = "FranquiciaCompletaBEG"
   }
 }
