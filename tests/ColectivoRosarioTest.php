290af8197bbb7daecd9533931ad594177498a544
Lorenzo393 <loloc.gelos@outlook.com>
2023-10-16 22:33:12 -0300

    ARREGLO TEST PAGAR CON

--- tests/ColectivoRosarioTest.php
+++ tests/ColectivoRosarioTest.php
@@ -43,34 +43,6 @@ class ColectivoRosarioTest extends TestCase{
         $this->assertEquals($tarjeta->saldo, -60);
     }
 
-    public function pagarCon($tarjeta , $tiempo){
-		$tipo_tarjeta = comprobar_tarjeta($tarjeta , $tiempo);
-		
-		if(tipo_tarjeta == 1){//Normal
-            $boleto = pagar_comun($tarjeta);
-            $tarjeta->usos_por_mes += 1;
-			return boleto;
-		}
-		if(tipo_tarjeta == 2){//Jubilados
-            $boleto = pagar_jubilado($tarjeta);
-			return boleto;
-		}
-		if(tipo_tarjeta == 3){//BEG
-            $boleto = pagar_beg($tarjeta);
-            $tarjeta->usos_por_dia += 1;
-			return boleto;
-		}
-		if(tipo_tarjeta == 4){//Parcial
-            $boleto = pagar_parcial($tarjeta);
-            $tarjeta->usos_por_dia += 1;
-			return boleto;
-		}
-        else{
-            return false;
-        }
-	}
-
-
     public function testComprobar_tarjeta(){
         $colectivo = new Colectivo_Rosario();
 
