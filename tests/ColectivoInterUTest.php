788ef9dded2c9abb5351d1158ccf4cdbfb71af8b
Lorenzo393 <loloc.gelos@outlook.com>
2023-10-16 23:59:01 -0300

    Arreglo interU

--- tests/ColectivoInterUTest.php
+++ tests/ColectivoInterUTest.php
@@ -5,12 +5,12 @@ namespace TrabajoSube;
 use PHPUnit\Framework\TestCase;
 
 class ColectivoInterUTest extends TestCase{
-    
+
     public function testPagarCon(){
         $colectivo = new Colectivo_InterU();
 
         $tarjeta = new Tarjeta(184);
         $colectivo->pagarCon($tarjeta,1535563577);
-        $this->assertEquals($tarjeta->saldo, 0);
-}
+        $this->assertEquals($tarjeta->saldo,0);
+    }
 }
